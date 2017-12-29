ad9c96d12c7e7ff9b1f8acd8a0f2fac8d4b2f08e
Shivani Mathur <qwellery@gmail.com>
2017-12-29 04:55:43 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,6 +5,11 @@ def my_all?(collection)
   block_return_values = []
   while i < collection.length
     block_return_values<<yield(collection[i])
-    i = i + 1  
+    i = i + 1
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
   end
 end
